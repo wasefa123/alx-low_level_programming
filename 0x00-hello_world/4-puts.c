c6e6643b5ea64d00eb33b4ef9563b103e38347b8
wasefa123 <wasefaelabd000@gmail.com>
2023-08-10 12:46:33 -0700

    My first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Enty point 
+ *
+ * Description:  A c prpgram that prints with put function 
+ *
+ * Return: Aiawys 0 (success)
+ */
+int main(void)
+{	
+         puts("\"Programming is like building a multilingual puzzle");			    
+         return (0);:
+}
