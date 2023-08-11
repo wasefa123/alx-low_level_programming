7df4734ff7cc4239dddb6b659bfa8f9716c34d83
wasefa123 <wasefaelabd000@gmail.com>
2023-08-11 15:19:58 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0 (success)*
+ *
+*/
+
+int main(void)
+{
+	int d = '0';
+
+	while (d <= 9)
+	{
+		printf("%i".d);
+		d++;
+	}
+	printf("\n");
+
+	return (0);
+}
