73bbb900aaebcb505b7fbdc97ba00bafd5a90645
wasefa123 <wasefaelabd000@gmail.com>
2023-08-15 09:07:23 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: prints the alphabet, in lowercase in new line
+ *
+*/
+
+void void print_alphabet(void)
+{
+	int w;
+
+	for (w = 'a'; w <= 'z'; w++)
+		_putchar(w);
+	_putchar('\n');
+}
