b1d51a00ee1822d9d92d5ea248cd962244b46bcb
wasefa123 <wasefaelabd000@gmail.com>
2023-08-20 14:12:04 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * jack_bauer - that prints every minute of the day of Jack Bauer
+*/
+
+void jack_bauer(void)
+{
+	int m, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m / 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
