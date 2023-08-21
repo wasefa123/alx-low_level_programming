bfc12095174f4054b698e7f67d281159764b04fc
wasefa123 <wasefaelabd000@gmail.com>
2023-08-21 15:20:43 -0700

    My first commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
  * puts2 - function that prints every other character of a string,
  *         starting with the first character, followed by a new line
  *
- * str: input string
+ * @str: input string
  *
  * Return: nothing to return
 */
