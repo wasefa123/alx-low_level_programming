608e6e4b807dd5c38892e052bc667da249a6b3a0
wasefa123 <wasefaelabd000@gmail.com>
2023-08-24 03:40:57 -0700

    My first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _strcat - function that concatenat two string
+ *
+ * @dest: dastination input pointer
+ * @src: source input pointer
+ *
+ * Return: pointer to @dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int w, w2;
+
+	w = 0;
+	while (des[w])
+		w++;
+	for (w2 = 0; str[w2] ; w2++)
+		dest[w2++] = src[w2]
+	return (dest);
+}
